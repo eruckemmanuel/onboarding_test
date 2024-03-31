eedb3b878fac2963e9f62cd68ab2401351a3155b
Eruck Emmanuel <eruck@vehseh.com>
2024-03-31 21:52:10 +0100

    #1 Added API endpoint to list progress with simple UI display

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from training.models import (Activity, UserActivity, UserActivityLog)
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at', 'is_active')
+
+
+@admin.register(UserActivity)
+class UserActivityAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'user', 'created_at', 'updated_at', 'completed')
+
+
+@admin.register(UserActivityLog)
+class UserActivityLogAdmin(admin.ModelAdmin):
+    list_display = ('user_activity', 'created_at', 'started_at', 'ended_at', 'score')
